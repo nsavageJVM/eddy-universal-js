b20a422360568253960663aff9d7da31a88caad8
nsavagejvm <nscode1@gmail.com>
2016-11-01 06:18:17 +0200

    update

--- eddys-universal-ui-four/components/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Home = () => ( <div> <h1>Welcome to eddy's ticketing app</h1> <p>Check out the latest tickets!</p> </div>)
-
-
-export default Home
-
--- eddys-universal-ui-four/components/Ticket.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Ticket = ({title, content}) => ( <div> <h3>{title}</h3> <p>{content}</p> </div>)
-
-export default Ticket
